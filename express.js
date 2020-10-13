7270c03800fa7add7da6c10d9bac2f8e2c14e3c6
forsythrl <tarrantrl@utexas.edu>
2020-10-12 20:22:19 -0500

    add api routes and get and post

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // require the routes
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 // set up the port variable
 const PORT = process.env.PORT || 3001;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.static('public'));
 // set up the routes
 app.use('/', htmlRoutes);
+app.use('/api', apiRoutes)
 
 // set up server to listen on the specified port
 app.listen(PORT, () => {
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,15 +1,28 @@
-// require path and router
-const path = require('path');
+// require router
 const router = require('express').Router();
+// require the validateNote function
+const {validateNote, createNewNote} = require('../../lib/notes');
+// require the note data
+const notes = require('../../db/db.json');
 
-// set up the root path for index.html
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+// set up the get request for the notes api
+router.get('/notes', (req, res) => {
+    res.json(notes);
 })
 
-// set up the path for notes.html
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+// set up the post request for the notes api
+router.post('/notes', (req, res) => {
+    // create an id based on the length of the current notes list
+    req.body.id = notes.length.toString();
+    // check that the note submitted is valid
+    if (!validateNote(req.body)){
+        // if the note is not valid, send a 400 error to indicate user error
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add the note to the data file
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 })
 
 module.exports = router;
